af7cd2e483ec0300d31b6c8773993c1c910cad6d
Sebastian Casallas <sebastiancasallasarias2020@gmail.com>
2026-08-09 11:18:51 -0500

    envs config

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+
+import { envs } from "./config/plugins/envs.plugin";
 import { ServerApp } from "./presentation/server";
 
 (async() => {
@@ -6,5 +8,6 @@ import { ServerApp } from "./presentation/server";
 
 
 function main(){
-    ServerApp.start();
+    // ServerApp.start();
+    console.log(envs)
 }
